9c84f4303ffb325f55becbda651af428b053e120
LUM <lumlum177@gmail.com>
2023-02-09 13:03:59 +0300

    Начальный коммит

---
+++ src/main/java/ru/hogwarts/school/Homework54Application.java
@@ -0,0 +1,13 @@
+package ru.hogwarts.school;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Homework54Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Homework54Application.class, args);
+    }
+
+}
